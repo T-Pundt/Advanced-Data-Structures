528c7416d2ccce9612321226407277bafa1acc1c
Tim <tim.pundt@trojans.dsu.edu>
2024-01-23 18:42:35 -0600

    Homework 1 Update: Documentation added

--- Homework 1/Homework 1 - Dynamic Arrays.py
+++ Homework 1/Homework 1 - Dynamic Arrays.py
@@ -1,8 +1,21 @@
+# This program was made with chatgpt and a reference to a subreddit
+# The link to the reddit post is
+# https://www.reddit.com/r/learnpython/comments/xjn9pq/opening_a_text_file_in_the_same_directory/
+# The rest of the program was made with chat gpt
+# The prompts used to make the program were:
+#   -what is a dynamic array in python
+#   -How to read a excel file with multiple rows and columns into a list in python
+#   -will this work with a csv file
+#   -I did that, and it works besides it excludes the first row of data which is located in cell 1A to 1H
+#   -How would I sort the data in the list of lists in alphabetical order of a select portion of data
+#   -Why lamdbda
+
 def main():
     import pandas as pd
     from pathlib import Path
 
     # This section of code creates the file pathway that is later used to open the csv file
+    # Reference to reddit post was made here
     root_dir = Path(__file__).parent
     file = root_dir / 'us-contacts (2).csv'
 
